c370aec442500c56b14538747fe51604e982f281
SilverFlame83 <imren_83@abv.bg>
2023-01-26 14:53:25 +0200

    Applaying custom hook to BasicForm

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,10 +1,48 @@
+import useInput from "../hooks/use-input";
+
 const BasicForm = (props) => {
+  const {
+    value: enteredFirsrtName,
+    isValid: enteredFirstNameIsValid,
+    hasError: firstNameInputHasError,
+    valueChangeHandler: firstNameChangeHandler,
+    inputBlurHandler: firstNameBlurHandler,
+    reset: resetFirstNameInput,
+  } = useInput((value) => value.trim() !== "");
+
+  let userFormIsValid = false;
+
+  if (enteredFirstNameIsValid) {
+    userFormIsValid(true);
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (!enteredFirsrtName) {
+      return;
+    }
+
+    resetFirstNameInput();
+  };
+
+  const firstNameClasses = firstNameInputHasError
+    ? "form-control invalid"
+    : "form-control";
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="control-group">
-        <div className="form-control">
+        <div className={firstNameClasses}>
           <label htmlFor="name">First Name</label>
-          <input type="text" id="name" />
+          <input
+            type="text"
+            id="name"
+            onChange={firstNameChangeHandler}
+            onBlur={firstNameBlurHandler}
+            value={enteredFirsrtName}
+          />
+          {firstNameInputHasError && <p className="error-text">Please enter first name!</p>}
         </div>
         <div className="form-control">
           <label htmlFor="name">Last Name</label>
